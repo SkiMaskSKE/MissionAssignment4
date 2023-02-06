99933fdd230fc2b934508ec9e73ccbf7c73b66d5
SELASSIE ESIAPE <esiapekjr@gmail.com>
2023-02-06 16:16:02 -0700

    Mission5

---
+++ Mission4/Models/GradeCalculatorModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required]
+        [Range(0, 100)]
+        public int Assign { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Group { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Quiz { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Midterm { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Final { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Intex { get; set; }
+    }
+}
